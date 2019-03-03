b2eb26921b42515786850fcfaf2fb78f09200c49
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-03-03 20:46:24 +0100

    java class cleaned

--- core/src/com/piotrdawidziuk/flappygame/FlappyGame.java
+++ core/src/com/piotrdawidziuk/flappygame/FlappyGame.java
@@ -9,24 +9,14 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class FlappyGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+
 	@Override
-	public void create () {
+	public void create() {
 		batch = new SpriteBatch();
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void render() {
+
 	}
 }
