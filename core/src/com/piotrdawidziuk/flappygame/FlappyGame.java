180bd8a2f3fcc7ef809f2e2a27491d410d00065f
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-03-03 21:21:39 +0100

    bird centered

--- core/src/com/piotrdawidziuk/flappygame/FlappyGame.java
+++ core/src/com/piotrdawidziuk/flappygame/FlappyGame.java
@@ -9,17 +9,20 @@ public class FlappyGame extends ApplicationAdapter {
 	SpriteBatch batch;
 
 	Texture background;
+	Texture bird;
 
 	@Override
 	public void create() {
 		batch = new SpriteBatch();
 		background = new Texture("bg.png");
+		bird = new Texture("bird.png");
 	}
 
 	@Override
 	public void render() {
 		batch.begin();
 		batch.draw(background,0,0, Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+		batch.draw(bird,Gdx.graphics.getWidth()/2 - bird.getWidth()/2,Gdx.graphics.getHeight()/2-bird.getHeight()/2);
 		batch.end();
 
 
